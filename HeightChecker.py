c4472df895819f2ebb8bc97126bfc755b8349384
manthansubhash01 <manthanziman20@gmail.com>
2025-02-16 16:33:49 +0530

    day26_question1

--- HeightChecker.py
+++ HeightChecker.py
@@ -41,10 +41,10 @@ Constraints:
 ##########################################################################
 
 class Solution:
-    def heightChecker(self, heights):
+    def heightChecker(self, heights) ->int:
         expected = sorted(heights)
         count = 0
         for i in range(len(heights)):
             if heights[i] != expected[i]:
                 count += 1
-        return count 
+        return count

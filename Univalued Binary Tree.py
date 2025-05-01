9cc86d1a86f0d84f541e2a0f56a6f7667094b038
manthansubhash01 <manthanziman20@gmail.com>
2025-05-01 10:50:03 +0530

    day 90

---
+++ Univalued Binary Tree.py
@@ -0,0 +1,67 @@
+# 965. Univalued Binary Tree
+###########################################################################
+'''
+Accepted
+Accepted
+Editorial
+Editorial
+Solutions
+Solutions
+Submissions
+Submissions
+Code
+Testcase
+Testcase
+Test Result
+965. Univalued Binary Tree
+Solved
+Easy
+Topics
+Companies
+
+A binary tree is uni-valued if every node in the tree has the same value.
+
+Given the root of a binary tree, return true if the given tree is uni-valued, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: root = [1,1,1,1,1,null,1]
+Output: true
+
+Example 2:
+
+Input: root = [2,2,2,5,2]
+Output: false
+
+ 
+
+Constraints:
+
+    The number of nodes in the tree is in the range [1, 100].
+    0 <= Node.val < 100
+'''
+###########################################################################
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
+        def preorder(root,lst):
+            if root is None:
+                return 
+            lst.append(root.val)
+            preorder(root.left,lst)
+            preorder(root.right,lst)
+        ele = root.val
+        lst = []
+        preorder(root,lst)
+        for i in lst:
+            if i != ele:
+                return False
+        return True

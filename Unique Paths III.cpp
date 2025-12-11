f535347826663659479025caba83785100bf5971
manthansubhash01 <manthanziman20@gmail.com>
2025-12-11 23:58:08 +0530

    day 308

---
+++ Unique Paths III.cpp
@@ -0,0 +1,103 @@
+// 980. Unique Paths III
+// Solved
+// Hard
+// Topics
+// premium lock iconCompanies
+
+// You are given an m x n integer array grid where grid[i][j] could be:
+
+//     1 representing the starting square. There is exactly one starting square.
+//     2 representing the ending square. There is exactly one ending square.
+//     0 representing empty squares we can walk over.
+//     -1 representing obstacles that we cannot walk over.
+
+// Return the number of 4-directional walks from the starting square to the ending square, that walk over every non-obstacle square exactly once.
+
+ 
+
+// Example 1:
+
+// Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,2,-1]]
+// Output: 2
+// Explanation: We have the following two paths: 
+// 1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2)
+// 2. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2)
+
+// Example 2:
+
+// Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,0,2]]
+// Output: 4
+// Explanation: We have the following four paths: 
+// 1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2),(2,3)
+// 2. (0,0),(0,1),(1,1),(1,0),(2,0),(2,1),(2,2),(1,2),(0,2),(0,3),(1,3),(2,3)
+// 3. (0,0),(1,0),(2,0),(2,1),(2,2),(1,2),(1,1),(0,1),(0,2),(0,3),(1,3),(2,3)
+// 4. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2),(2,3)
+
+// Example 3:
+
+// Input: grid = [[0,1],[2,0]]
+// Output: 0
+// Explanation: There is no path that walks over every empty square exactly once.
+// Note that the starting and ending square can be anywhere in the grid.
+
+ 
+
+// Constraints:
+
+//     m == grid.length
+//     n == grid[i].length
+//     1 <= m, n <= 20
+//     1 <= m * n <= 20
+//     -1 <= grid[i][j] <= 2
+//     There is exactly one starting cell and one ending cell.
+
+class Solution {
+public:
+    int m, n, totalCells, ans = 0;
+    
+    void dfs(vector<vector<int>>& grid, int x, int y, int count) {
+        if (grid[x][y] == 2) {
+            if (count == totalCells) ans++;
+            return;
+        }
+        
+        int temp = grid[x][y];
+        grid[x][y] = -1; 
+        
+        int dx[4] = {1, -1, 0, 0};
+        int dy[4] = {0, 0, 1, -1};
+        
+        for (int k = 0; k < 4; k++) {
+            int nx = x + dx[k], ny = y + dy[k];
+            
+            if (nx >= 0 && ny >= 0 && nx < m && ny < n 
+                && grid[nx][ny] != -1) 
+            {
+                dfs(grid, nx, ny, count + 1);
+            }
+        }
+        
+        grid[x][y] = temp; 
+    }
+    
+    int uniquePathsIII(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        
+        int sx, sy;
+        totalCells = 0;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] != -1) totalCells++;
+                if (grid[i][j] == 1) {
+                    sx = i;
+                    sy = j;
+                }
+            }
+        }
+        
+        dfs(grid, sx, sy, 1);
+        return ans;
+    }
+};
